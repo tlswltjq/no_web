0870067fed589f7af9910f27e44009e62764f620
tlswltjq <wltjq1203@icloud.com>
2023-05-02 10:06:20 +0900

    Initial Commit

---
+++ no_web/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  no_web
+//
+//  Created by 신지섭 on 2023/05/02.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
