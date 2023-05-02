8a91ca7488515b1b3b496615d6916de41d5d55c5
tlswltjq <wltjq1203@icloud.com>
2023-05-02 15:18:31 +0900

    TIL CPP 1챕터 정리

--- no_web/main.cpp
+++ no_web/main.cpp
@@ -7,23 +7,19 @@
 
 #include <iostream>
 using namespace std;
-#define SQUARE(x) ((x) * (x))
-int sqrt();
-inline int sqrtt(int x){
-    return x * x;
-}
-namespace Parent{
-    int num = 2;
-    namespace child1 {
-    int num = 3;
+namespace parent{
+int num = 2;
+    namespace child1{
+        int num =4;
     }
-    namespace child2 {
-    int num = 4;
+    namespace child2{
+        int num =3;
     }
 }
-int main(int argc, const char * argv[]) {
-    cout<<"ParentNum : "<<Parent::num<<endl;
-    cout<<"Child1Num : "<<Parent::child1::num<<endl;
-    cout<<"Child2Num : "<<Parent::child2::num<<endl;
-    return 0;
-}
+    int main(void){
+        cout<<parent::num<<"\n";
+        cout<<parent::child1::num<<"\n";
+        cout<<parent::child2::num<<"\n";
+        
+        return 0;
+    }
